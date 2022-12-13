7f4a5c09d0455faab69aaf7ab154625ddc9a99af
sangwon <swchoi1997@naver.com>
2022-12-13 16:42:41 +0900

    feat: 기본조인

--- src/test/java/com/example/querydsl/QueryDslBasicTest.java
+++ src/test/java/com/example/querydsl/QueryDslBasicTest.java
@@ -201,6 +201,19 @@ public class QueryDslBasicTest {
 
         assertThat(teamB.get(team.name)).isEqualTo("teamB");
         assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+    }
 
+    //join
+    @Test
+    public void defaultJoin() throws Exception{
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
     }
 }
