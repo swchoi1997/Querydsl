37997f260e2caa4b5ad9ad873fd9dae7d2939f52
sangwon <swchoi1997@naver.com>
2022-12-20 15:19:58 +0900

    feat: 조회 api 컨트롤러 개발

---
+++ src/main/java/com/example/querydsl/controller/MemberController.java
@@ -0,0 +1,22 @@
+package com.example.querydsl.controller;
+
+import com.example.querydsl.dto.MemberSearchCondition;
+import com.example.querydsl.dto.MemberTeamDto;
+import com.example.querydsl.repository.MemberJpaRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+        return memberJpaRepository.searchByWhere(condition);
+    }
+}
